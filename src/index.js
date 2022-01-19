a8a8ae9d11e2c77c8f54b3aee7d78389efc481c8
Dalton <slattery.da@gmail.com>
2022-01-19 09:59:58 -0600

    initial fetching

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 console.log('%c HI', 'color: firebrick')
+const imgUrl = 'https://dog.ceo/api/breeds/image/random/4'
+
+fetch(imgUrl)
+    .then(response => response.json())
+    .then(data => {
+        data.message.forEach((imgLink) =>{
+            let imgTag = document.createElement('img')
+            imgTag.src = imgLink
+            document.getElementById('dog-image-container').append(imgTag)
+        })
+    })   
+   
