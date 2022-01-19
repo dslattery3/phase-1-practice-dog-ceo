ce66d59511f2acf31dcf1bdcbd5a519d849b89f7
johnlemak <lemak91@mac.com>
2022-01-19 10:29:46 -0600

    challenge 2

--- src/index.js
+++ src/index.js
@@ -1,13 +1,32 @@
 console.log('%c HI', 'color: firebrick')
 const imgUrl = 'https://dog.ceo/api/breeds/image/random/4'
+const breedUrl = 'https://dog.ceo/api/breeds/list/all'
 
-fetch(imgUrl)
+// fetch(imgUrl)
+//     .then(response => response.json())
+//     .then(data => {
+//         data.message.forEach((imgLink) =>{
+//             let imgTag = document.createElement('img')
+//             imgTag.src = imgLink
+//             document.getElementById('dog-image-container').append(imgTag)
+//         })
+//     })   
+fetch(breedUrl)
     .then(response => response.json())
     .then(data => {
-        data.message.forEach((imgLink) =>{
-            let imgTag = document.createElement('img')
-            imgTag.src = imgLink
-            document.getElementById('dog-image-container').append(imgTag)
-        })
-    })   
-   
+        console.log(data)
+
+        // Object.keys(data.message).forEach(breed =>{
+        for(let breed in data.message) {
+            let ulTag = document.createElement('ul')
+            ulTag.innerText = `${breed}`
+            document.getElementById('dog-breeds').append(ulTag) 
+
+        }
+        // data.message.forEach((breed) =>{
+        //     let ulTag = document.createElement('ul')
+        //     ulTag = breed
+        //     document.getElementById('dog-breeds').append(ulTag)
+            // console.log(breed)
+        // })
+    })    
